99cbcf71eb07d84871c4daed9007dfd12b127b71
HiiFyi <167199917+HiiFyi@users.noreply.github.com>
2025-06-02 22:40:20 +0530

    Update info.py

--- info.py
+++ info.py
@@ -55,7 +55,8 @@ UPDATE_CHANNEL_LNK = environ.get('UPDATE_CHANNEL_LNK', 'https://t.me/')
 
 #Force Subscription Channel (Put Same Channel Id In Both Veriables)
 AUTH_CHANNEL = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('AUTH_CHANNEL', '').split()] 
-AUTH_REQ_CHANNEL = int(environ.get('AUTH_REQ_CHANNEL', '-100'))
+AUTH_REQ_CHANNEL = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('AUTH_REQ_CHANNEL', '').split()] 
+#AUTH_REQ_CHANNEL = int(environ.get('AUTH_REQ_CHANNEL', '-100'))
 
 IS_VERIFY = is_enabled('IS_VERIFY', True)
 LOG_VR_CHANNEL = int(environ.get('LOG_VR_CHANNEL', '-100')) #Verification Channel Id 
